dceaba2d91eb889274f74b8d5dbb7e1864c38413
MilosMiki <miki2000milos@gmail.com>
2025-04-03 23:06:13 +0200

    added flags

---
+++ src/currencyToCountry.js
@@ -0,0 +1,15 @@
+// currencyToCountry.js
+// Maps your specific currencies to country flags
+export const currencyToCountry = {
+  // Your active currencies
+  "EUR": "EU",   // Euro (European Union flag)
+  "USD": "US",   // US Dollar
+  "CHF": "CH",   // Swiss Franc
+  "GBP": "GB",   // British Pound (UK flag)
+  "BAM": "BA",   // Bosnia-Herzegovina Convertible Mark
+  "HRK": "HR",   // Croatian Kuna (Croatia)
+  "AUD": "AU",   // Australian Dollar
+  "CAD": "CA",   // Canadian Dollar
+  "HUF": "HU",   // Hungarian Forint
+  "NOK": "NO",   // Norwegian Krone
+};
